bb92e9cfd67ad8bffe17a0a22bb50be1c9502db0
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2013-02-28 13:23:11 -0500

    Copyright notices

--- src/org/kirill/syntopiary/SyntopWindow.java
+++ src/org/kirill/syntopiary/SyntopWindow.java
@@ -1,3 +1,24 @@
+/* Copyright 2013 Kirill Shkovsky
+ * 
+ * This file is part of SynTopiary.
+ * 
+ * SynTopiary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SynTopiary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SynTopiary.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Portions of this software are under Apache Software License, 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ */
 package org.kirill.syntopiary;
  
 import java.awt.Desktop;
