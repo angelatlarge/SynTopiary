282b99a06f10d14710914c379990b1efe977eda3
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2013-03-01 14:58:40 -0500

    Feedback form seems to work OK

--- deploy/scripts/syntopiary_feedback.php
+++ deploy/scripts/syntopiary_feedback.php
@@ -1,11 +1,18 @@
 <?php
 
-	$message = 
-			$_POST['name'] . "\n"
-		. 	$_POST['email'] . "\n"
-		. 	$_POST['feedback'] . "\n"
-		. 	"\n" . print_r($_POST, true) . "\n"
-		. 	"\n" . print_r($_GET, true) . "\n";
+	$message = ""
+		.	"Name: "
+			. preg_replace('=((<CR>|<LF>|0x0A/%0A|0x0D/%0D|\\n|\\r)\S).*=i', null, $_POST['name'])
+			. "\n"
+		. 	"Email: " 
+			. preg_replace('=((<CR>|<LF>|0x0A/%0A|0x0D/%0D|\\n|\\r)\S).*=i', null, $_POST['email'])
+			. "\n"
+		.	"Comments\n\n"
+			. preg_replace('=((<CR>|<LF>|0x0A/%0A|0x0D/%0D|\\n|\\r)\S).*=i', null, $_POST['feedback'])
+			. "\n"
+		//~ . 	"\n" . print_r($_POST, true) . "\n"
+		//~ . 	"\n" . print_r($_GET, true) . "\n"
+		;
 	mail ( 
 		"kirills@mit.edu", 
 		"Syntopiary feedback", 
