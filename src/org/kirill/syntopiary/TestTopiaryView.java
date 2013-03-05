4d4bd04ce14887afcc0bfb44f2d18c1e5a251d33
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2013-03-05 14:29:26 -0500

    Now layout test is set up pretty well

--- src/org/kirill/syntopiary/TestTopiaryView.java
+++ src/org/kirill/syntopiary/TestTopiaryView.java
@@ -1,20 +1,60 @@
 package org.kirill.syntopiary;
 
-public class TestTopiaryView {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-	public static void testLayoutSimple() {
-		String[] args;
-		
-		// Parent node larger than child node
-		args = new String[3];
-		args[0] = "--treespec=AAAAAAAAAAAAA(B)";
-		args[1] = "--svgfile=test_output_1.svg";
+public class TestTopiaryView {
+	
+	private final String imageNameTemplate_T = "test_output_%d.svg";
+	private final String htmlFilename = "TestResults.html";
+	private int idxCurrentImage = 0;
+	private StringBuilder htmlOutput = new StringBuilder();
+	
+	protected void makeTreeImage(String specification) throws InterruptedException {
+		String[] args = new String[3];
+		args[0] = String.format("--treespec=%s", specification);
+		String fileSpec = String.format(imageNameTemplate_T, idxCurrentImage++);
+		args[1] = String.format("--svgfile=%s", fileSpec);
 		args[2] = "--terminate=true";
 		MainApp.main(args);
+		htmlOutput.append(String.format("<P>%s\n", specification));
+		htmlOutput.append(String.format("<BR><IMG SRC=\"%s\" style=\"border: 1px solid red\">\n", fileSpec));
+		Thread.sleep(1000);
+	}
+	
+	
+	protected void testLayoutSimple() throws InterruptedException {
+		
+		// Parent node larger than child node
+		makeTreeImage("AAAAAAAAAAAAAAAAA(B)");
+		makeTreeImage("AAAAAAAAAAAAAAAAA(B(C))");
+		makeTreeImage("AAAAAAAAAAAAAAAAA(B(C, D))");
 		
 	}
-	public static void main(String[] args) {
-		testLayoutSimple();
+	
+	
+	public void testAll() throws InterruptedException, IOException {
+		htmlOutput.append("<html>\n<body>\n");
+		try	{
+			testLayoutSimple();
+			htmlOutput.append("</body>\n<html>\n");
+		} finally {
+			// Make an HTML output file
+			FileOutputStream fos = new FileOutputStream(new File(htmlFilename));
+			OutputStreamWriter writerOut = new OutputStreamWriter(fos, "UTF-8");
+			writerOut.write(htmlOutput.toString());
+			writerOut.close();
+			fos.close();
+		}
+		Runtime.getRuntime().exec(String.format("firefox %s", htmlFilename));
+	}
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		TestTopiaryView ttv = new TestTopiaryView();
+		ttv.testAll();
 	}
 
 }
